d292c039bd390533cd6c728e3df6e78ab7e8b504
jvsm123 <jvsm123@outlook.com>
2021-11-02 08:51:24 -0300

    terminando CSS

--- src/comp/Perfil/Perfil.tsx
+++ src/comp/Perfil/Perfil.tsx
@@ -20,7 +20,7 @@ export default class Perfil extends React.Component< Props, {} >
         const meses: Array<String> = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
         const date: Date = new Date(reg.join(''));
         const created_at: String = ( (date.getDate()) + " " + (meses[date.getMonth()]) + " " + (date.getFullYear()) )
-     
+
         return (
             <div className="perfil">
                 <aside>
@@ -30,66 +30,64 @@ export default class Perfil extends React.Component< Props, {} >
                         alt="#"
                     />
                 </aside>
-             
-                <div className="perfilAspec">
-                    <div className="detalhes">
-                        <div className="user">
-                            <h2>{this.props.user.login}</h2>
-                            <p>{this.props.user.name}</p>
-                        </div>
-                        <p>Joined {created_at}</p>
+
+                <div className="detalhes">
+                    <div className="user">
+                        <h2>{this.props.user.login}</h2>
+                        <p>{this.props.user.name}</p>
                     </div>
-                 
-                    <div className="desc">
-                        <h4>{this.props.user.bio}</h4>
+                    <p>Joined {created_at}</p>
+                </div>
+
+                <div className="desc">
+                    <h4>{this.props.user.bio}</h4>
+                </div>
+
+                <div className="gitinfos">
+                    <div className="repos">
+                        <h4>Repos</h4>
+                        <h3>{this.props.user.public_repos}</h3>
                     </div>
-                 
-                    <div className="gitinfos">
-                        <div className="repos">
-                            <h4>Repos</h4>
-                            <h3>{this.props.user.public_repos}</h3>
+                    <div className="followers">
+                        <h4>Followers</h4>
+                        <h3>{this.props.user.followers}</h3>
+                    </div>
+                    <div className="following">
+                        <h4>Following</h4>
+                        <h3>{this.props.user.following}</h3>
+                    </div>
+                </div>
+
+                <div className="media">
+                    <div className="left">
+                        <div className="mediaInfo">
+                            <p>
+                                <img src={Geo} alt="Geo"/>
+                                {geo}
+                            </p>
                         </div>
-                        <div className="followers">
-                            <h4>Followers</h4>
-                            <h3>{this.props.user.followers}</h3>
-                        </div>
-                        <div className="following">
-                            <h4>Following</h4>
-                            <h3>{this.props.user.following}</h3>
+
+                        <div className="mediaInfo end">
+                            <p>
+                                <img src={Link} alt="Link"/>
+                                {link}
+                            </p>
                         </div>
                     </div>
-                 
-                    <div className="media">
-                        <div className="left">
-                            <div className="mediaInfo">
-                                <p>
-                                    <img src={Geo} alt="Geo"/>
-                                    {geo}
-                                </p>
-                            </div>
-                         
-                            <div className="mediaInfo end">
-                                <p>
-                                    <img src={Link} alt="Link"/>
-                                    {link}
-                                </p>
-                            </div>
+
+                    <div className="right">
+                        <div className="mediaInfo">
+                            <p>
+                                <img src={Twitter} alt="Twitter"/>
+                                {tw}
+                            </p>
                         </div>
-                     
-                        <div className="right">
-                            <div className="mediaInfo">
-                                <p>
-                                    <img src={Twitter} alt="Twitter"/>
-                                    {tw}
-                                </p>
-                            </div>
-                         
-                            <div className="mediaInfo end">
-                                <p>
-                                    <img src={Build} alt="Build"/>
-                                    {comp}
-                                </p>
-                            </div>
+
+                        <div className="mediaInfo end">
+                            <p>
+                                <img src={Build} alt="Build"/>
+                                {comp}
+                            </p>
                         </div>
                     </div>
                 </div>
--- src/comp/Perfil/PerfilFake.tsx
+++ src/comp/Perfil/PerfilFake.tsx
@@ -19,70 +19,68 @@ export default class Perfil extends Component
                         alt="#"
                     />
                 </aside>
-             
-                <div className="perfilAspec">
-                    <div className="detalhes">
-                        <div className="user">
-                            <h2>The Octocat</h2>
-                            <p>octocat</p>
-                        </div>
-                        <p>Joined 25 Jan 2011</p>
+
+                <div className="detalhes">
+                    <div className="user">
+                        <h2>The Octocat</h2>
+                        <p >octocat</p>
                     </div>
-                 
-                    <div className="desc">
-                        <h4>
-                            Dolor ea quidem aut corporis labore?
-                            Ipsa fugit rem iure voluptas explicabo
-                            Molestiae facilis dolores quam neque
-                        </h4>
+                    <p>Joined 25 Jan 2011</p>
+                </div>
+
+                <div className="desc">
+                    <h4>
+                        Dolor ea quidem aut corporis labore?
+                        Ipsa fugit rem iure voluptas explicabo
+                        Molestiae facilis dolores quam neque
+                    </h4>
+                </div>
+
+                <div className="gitinfos">
+                    <div className="repos">
+                        <h4>Repos</h4>
+                        <h3>8</h3>
                     </div>
-                 
-                    <div className="gitinfos">
-                        <div className="repos">
-                            <h4>Repos</h4>
-                            <h3>8</h3>
+                    <div className="followers">
+                        <h4>Followers</h4>
+                        <h3>3938</h3>
+                    </div>
+                    <div className="following">
+                        <h4>Following</h4>
+                        <h3>9</h3>
+                    </div>
+                </div>
+
+                <div className="media">
+                    <div className="left">
+                        <div className="mediaInfo">
+                            <p>
+                                <img src={Geo} alt="Geo"/>
+                                San Francisco
+                            </p>
                         </div>
-                        <div className="followers">
-                            <h4>Followers</h4>
-                            <h3>3938</h3>
-                        </div>
-                        <div className="following">
-                            <h4>Following</h4>
-                            <h3>9</h3>
+
+                        <div className="mediaInfo end">
+                            <p>
+                                <img src={Link} alt="Link"/>
+                                https://github.blog
+                            </p>
                         </div>
                     </div>
-                 
-                    <div className="media">
-                        <div className="left">
-                            <div className="mediaInfo">
-                                <p>
-                                    <img src={Geo} alt="Geo"/>
-                                    San Francisco
-                                </p>
-                            </div>
-                         
-                            <div className="mediaInfo end">
-                                <p>
-                                    <img src={Link} alt="Link"/>
-                                    https://github.blog
-                                </p>
-                            </div>
+
+                    <div className="right">
+                        <div className="mediaInfo">
+                            <p>
+                                <img src={Twitter} alt="Twitter"/>
+                                Not Available
+                            </p>
                         </div>
-                     
-                        <div className="right">
-                            <div className="mediaInfo">
-                                <p>
-                                    <img src={Twitter} alt="Twitter"/>
-                                    Not Available
-                                </p>
-                            </div>
-                         
-                            <div className="mediaInfo end">
-                                <p>
-                                    <img src={Build} alt="Build"/>
-                                    agithub
-                                </p>
-                            </div>
+
+                        <div className="mediaInfo end">
+                            <p>
+                                <img src={Build} alt="Build"/>
+                                agithub
+                            </p>
                         </div>
                     </div>
                 </div>
